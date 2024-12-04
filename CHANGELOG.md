1f53d748ef7320d9056a5590e2d8ee6e8cbdd945
kane <justinl@kududyn.com>
2024-12-04 11:23:23 -0500

    Adds example tf deployment for crs architecture

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# CHANGELOG.md
+
+## 2024-12-03
+
+- Iniitla commit of example-crs-architecture
+- Added linter rules to root of `compint` repository
---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "example-tfstate-rg"
+    storage_account_name = "exampleserviceaccountname"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
